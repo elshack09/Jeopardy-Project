0fb476d1a9d29fc0f71e75f927b556c89b938c58
elshack09 <elshack09@gmail.com>
2018-05-23 18:51:12 -0400

    got button value to hide after clicked

--- javascript/jeopardy.js
+++ javascript/jeopardy.js
@@ -2,12 +2,16 @@ $(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
 
+// hide button after it's clicked function
 
-// function myName(){
-//     prompt("Enter Your Name Player");
-//     document.getElementById('devName').write
-// }
+$(document).ready(function(){
+    $("#aa1").click(function(){
+        $(this).hide();
+    });
+});
 
+
+// function value
 $(document).ready(function(){
     $("#a1").click(function(){
         $("#point").append().text("200");
