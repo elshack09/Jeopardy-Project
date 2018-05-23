ef8879cfe771fba3933cdc9461f23b250a6dbff6
elshack09 <elshack09@gmail.com>
2018-05-23 18:22:33 -0400

    got point value to display in score section

--- javascript/jeopardy.js
+++ javascript/jeopardy.js
@@ -1,3 +1,22 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+
+// function myName(){
+//     prompt("Enter Your Name Player");
+//     document.getElementById('devName').write
+// }
+
+$(document).ready(function(){
+    $("#a1").click(function(){
+        $("#point").append().text("200");
+    });
+    $("#b1").click(function(){
+        $("#point").append().text("-200");
+    });
+    $("#c1").click(function(){
+        $("#point").append().text("-200");
+    });
+
+});
